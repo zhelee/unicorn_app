c23c7224cf0989c3b59fee33a28661dc12bcd90b
Lizhe <macapple@localhost.(none)>
2013-01-13 10:38:42 +0800

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :test, only: [:index]
+  
+
+  def this
+  end
+
 end
---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :rvm_type, "system"
+set :rvm_string, "ruby-1.9.3-p327"
+
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+set :application, "unicorn_app"
+set :deploy_to, "/vagrant/www"
+set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :domain, "192.168.33.10"
+server domain, :web, :app, :db, :primary => true
+set :user, "vagrant"
+set :use_sudo, false
+
+set :scm, :git
+set :repository,  "git://github.com/zhelee/unicorn_app.git"
+set :branch, :master
+
+after "deploy:restart", "deploy:cleanup"
