89b2b771660e7112c249cd592e64506be8c1cb75
lizhe <me@lizhe.name>
2013-01-13 17:39:24 +0800

    update nginx config

--- deploy/recipes/nginx.rb
+++ deploy/recipes/nginx.rb
@@ -1,6 +1,7 @@
 namespace :nginx do
   task :setup do
-    template "nginx.conf.erb", "/etc/nginx/sites-enabled/unicorn_app.conf"
+    template "nginx.conf.erb", "/tmp/unicorn_app.conf"
+    run "#{sudo} mv /tmp/unicorn_app.conf /etc/nginx/sites-enabled/unicorn_app.conf"
   end
   %w{start stop restart}.each do |state|
     task state do
