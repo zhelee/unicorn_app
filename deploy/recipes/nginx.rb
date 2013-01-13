d88e3a5bfa36e650dfded7b857274f25719ed0de
lizhe <me@lizhe.name>
2013-01-13 14:42:40 +0800

    bugfix

--- deploy/recipes/nginx.rb
+++ deploy/recipes/nginx.rb
@@ -2,7 +2,7 @@ namespace :nginx do
   task :setup do
     template "nginx.conf.erb", "/etc/nginx/sites-enalbed/unicorn_app.conf"
   end
-  %{start stop restart}.each do |state|
+  %w{start stop restart}.each do |state|
     task state do
       "#{sudo} service nginx #{state}"
     end
--- deploy/recipes/unicorn.rb
+++ deploy/recipes/unicorn.rb
@@ -4,7 +4,7 @@ namespace :unicorn do
     template "unicorn.init.erb", "/etc/init.d/unicorn_#{application}"
     run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
   end
-  %{start stop restart}.each do |state|
+  %w{start stop restart}.each do |state|
     task state do
       "#{sudo} service unicorn #{state}"
     end
