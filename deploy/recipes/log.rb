b0269b763b2a0d58064a0722e4b2210c475d5ece
lizhe <me@lizhe.name>
2013-01-15 22:28:24 +0800

    update

--- deploy/recipes/log.rb
+++ deploy/recipes/log.rb
@@ -1,7 +1,41 @@
-desc "tail log files"
-task :tail_log, :roles => :app do
-  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
-    puts "#{channel[:host]}: #{data}"
-    break if stream == :err
+namespace :log do
+  desc "tail rails log files"
+  task :rails, :roles => :app do
+    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
+  end
+  
+  desc "tail unicorn out log files"
+  task :unicorn, :roles => :app do
+    run "tail -f #{shared_path}/log/unicorn_stdout.log" do |channel, stream, data|
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
+  end
+
+  desc "tail unicorn out log files"
+  task :unicorn_err, :roles => :app do
+    run "tail -f #{shared_path}/log/unicorn_stderr.log" do |channel, stream, data|
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
+  end
+
+  desc "tail nginx out log files"
+  task :nginx, :roles => :web do
+    run "tail -f /var/log/nginx/access.log" do |channel, stream, data|
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
+  end
+
+  desc "tail nginx out log files"
+  task :nginx_err, :roles => :web do
+    run "tail -f /var/log/nginx/error.log" do |channel, stream, data|
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
   end
 end
